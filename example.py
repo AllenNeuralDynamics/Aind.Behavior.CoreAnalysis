beba0d4604c04694c111b7c175b5675aae7739d3
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-03-31 15:38:33 -0700

    Move dataset pretty print method

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
-from aind_behavior_core_analysis.csv import CsvBuilder, CsvReaderParams, CsvWriterParams
-from aind_behavior_core_analysis.json import JsonBuilder, JsonReaderParams, JsonWriterParams
 from aind_behavior_core_analysis.base import DataStream
+from aind_behavior_core_analysis.csv import CsvBuilder, CsvReaderParams, CsvWriterParams
 from aind_behavior_core_analysis.dataset import Dataset, DataStreamGroup
+from aind_behavior_core_analysis.harp import DeviceYmlByRegister0, HarpDeviceBuilder, HarpDeviceReaderParams
+from aind_behavior_core_analysis.json import JsonBuilder, JsonReaderParams, JsonWriterParams
 
 my_dataset = Dataset(
     name="my_dataset",
@@ -28,42 +29,17 @@ my_dataset = Dataset(
                     )
                 }
             ),
+            "HarpBehavior": DataStreamGroup(
+                io=HarpDeviceBuilder,
+                reader_params=HarpDeviceReaderParams(
+                    path=r"C:\Users\bruno.cruz\Desktop\0123456789_2025-01-28T023311Z\behavior\Behavior.harp",
+                    device_yml_hint=DeviceYmlByRegister0(),
+                ),
+                writer_params=None,
+            ),
         }
     ),
 )
 
 
-def print_data_stream_tree(node, prefix="", *, exclude_params: bool = False):
-    """
-    Prints the structure of the data streams using emojis.
-    :param node: The current node (DatasetNode or DataStream).
-    :param prefix: The prefix for the tree structure.
-    """
-
-    icon_map = {
-        DataStream: "📄",
-        DataStreamGroup: "📂",
-    }
-
-    if isinstance(node, DataStream):
-        reader = node.io._reader.__name__ if node.io._reader else "None"
-        writer = node.io._writer.__name__ if node.io._writer else "None"
-        reader_params = node.reader_params if reader != "None" else ""
-        writer_params = node.writer_params if writer != "None" else ""
-        if exclude_params:
-            _str = f"{prefix}⬇️  {reader} \n" + f"{prefix}⬆️  {writer}\n"
-        else:
-            _str = (
-                f"{prefix}⬇️  {reader}: \n"
-                + f"{prefix}   <{reader_params}>\n"
-                + f"{prefix}⬆️  {writer}:\n"
-                + f"{prefix}   <{writer_params}>"
-            )
-        print(_str)
-    elif isinstance(node, DataStreamGroup):
-        for key, child in node.data_streams.items():
-            print(f"{prefix}{icon_map[type(child)]} {key}")
-            print_data_stream_tree(child, prefix + "    ", exclude_params=exclude_params)
-
-
-print_data_stream_tree(my_dataset.data_streams, exclude_params=True)
+my_dataset.print()
--- src/aind_behavior_core_analysis/dataset.py
+++ src/aind_behavior_core_analysis/dataset.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
+from typing import Literal
 
-from aind_behavior_core_analysis.base import DataStreamGroup
+from aind_behavior_core_analysis.base import DataStream, DataStreamGroup
 
 
 @dataclass
@@ -9,3 +10,76 @@ class Dataset:
     version: str
     description: str
     data_streams: DataStreamGroup
+
+    def print(self, exclude_params: bool = False, print_if_none: bool = False) -> None:
+        """
+        Prints the structure of the dataset using emojis.
+        :param exclude_params: If True, excludes the parameters from the output.
+        :param print_if_none: If True, prints "None" for None values.
+        """
+        print_data_stream_tree(self.data_streams, exclude_params=exclude_params, print_if_none=print_if_none)
+
+
+def print_data_stream_tree(
+    node: DataStreamGroup | DataStream, prefix="", *, exclude_params: bool = False, print_if_none: bool = False
+) -> None:
+    """
+    Prints the structure of the data streams using emojis.
+    :param node: The current node (DatasetNode or DataStream).
+    :param prefix: The prefix for the tree structure.
+    """
+
+    icon_map = {
+        DataStream: "📄",
+        DataStreamGroup: "📂",
+        None: "❓",
+        "reader": "⬇️",
+        "writer": "⬆️",
+    }
+
+    # if isinstance(node, DataStream):
+    def _print_io(
+        reader_or_writer,
+        params,
+        prefix: str,
+        io_type: Literal["reader", "writer"],
+        *,
+        print_if_none: bool = False,
+        exclude_params: bool = False,
+    ) -> str:
+        if not print_if_none and reader_or_writer is None:
+            return ""
+        io_name = reader_or_writer.__name__ if reader_or_writer else "None"
+        params = params if reader_or_writer else ""
+        _str = f"{prefix}{icon_map[io_type]}  {io_name} \n"
+        if not exclude_params:
+            _str += f"{prefix}   <{params}>\n"
+        return _str
+
+    s_builder = ""
+    s_builder += _print_io(
+        node.io._reader,
+        node.reader_params,
+        prefix,
+        "reader",
+        print_if_none=print_if_none,
+        exclude_params=exclude_params,
+    )
+    s_builder += _print_io(
+        node.io._writer,
+        node.writer_params,
+        prefix,
+        "writer",
+        print_if_none=print_if_none,
+        exclude_params=exclude_params,
+    )
+    s_builder = s_builder.rstrip("\n")
+    print(s_builder) if s_builder else None
+
+    if isinstance(node, DataStreamGroup):
+        if node._data_streams is None:
+            print(f"{prefix}{prefix}{icon_map[None]} Not loaded")
+        if node._data_streams is not None:
+            for key, child in node.data_streams.items():
+                print(f"{prefix}{icon_map[type(child)]} {key}")
+                print_data_stream_tree(child, prefix + "    ", exclude_params=exclude_params)
