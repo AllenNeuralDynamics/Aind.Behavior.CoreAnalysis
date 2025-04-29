83843a6d23fe2bc1c8e557d9e4193ac88eeb2d53
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-04-29 16:15:20 -0700

    Create utils module

--- src/aind_behavior_core_analysis/utils.py
+++ src/aind_behavior_core_analysis/utils.py
@@ -3,7 +3,7 @@ from typing import List, Optional, Tuple
 from ._core import DataStream, DataStreamGroup
 
 
-def load_branch(group: DataStreamGroup, strict: bool = False) -> Optional[List[Tuple[str, DataStream, Exception]]]:
+def load_branch(group: DataStreamGroup, strict: bool = False) -> Optional[List[Tuple[DataStream, Exception]]]:
     """Recursively load all data streams in the branch using breadth-first traversal.
 
     This method first loads the data for the current node, then proceeds to load
@@ -11,14 +11,14 @@ def load_branch(group: DataStreamGroup, strict: bool = False) -> Optional[List[T
     """
     if strict:
         group.load()
-        for _, stream in group.walk_data_streams():
+        for stream in group.walk_data_streams():
             stream.load()
         return None
     else:
         exceptions = []
-        for key, stream in group.walk_data_streams():
+        for stream in group.walk_data_streams():
             try:
                 stream.load()
             except Exception as e:
-                exceptions.append((key, stream, e))
+                exceptions.append((stream, e))
         return exceptions
