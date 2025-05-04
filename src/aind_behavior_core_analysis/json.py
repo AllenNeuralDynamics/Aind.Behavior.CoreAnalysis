6e223b812ec808bcbbe7b07afab3021f08c10e5a
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-05-03 17:03:38 -0700

    Add json

--- src/aind_behavior_core_analysis/json.py
+++ src/aind_behavior_core_analysis/json.py
@@ -7,6 +7,7 @@ import pandas as pd
 import pydantic
 
 from . import FilePathBaseParam
+from ._core import DataStream
 
 
 @dataclasses.dataclass
@@ -15,61 +16,27 @@ class JsonReaderParams:
     encoding: str = "UTF-8"
 
 
-def json_reader(params: JsonReaderParams) -> dict[str, str]:
-    with open(params.path, "r", encoding=params.encoding) as file:
-        data = json.load(file)
-    return data
+class Json(DataStream[dict[str, str], JsonReaderParams]):
+    @staticmethod
+    def _reader(params: JsonReaderParams) -> dict[str, str]:
+        with open(params.path, "r", encoding=params.encoding) as file:
+            data = json.load(file)
+        return data
+
+    parameters = JsonReaderParams
 
 
-@dataclasses.dataclass
-class JsonWriterParams:
-    path: os.PathLike
-    indent: int = 4
-    encoding: str = "UTF-8"
+class MultiLineJson(DataStream[list[dict[str, str]], JsonReaderParams]):
+    @staticmethod
+    def _reader(params: JsonReaderParams) -> list[dict[str, str]]:
+        with open(params.path, "r", encoding=params.encoding) as file:
+            data = [json.loads(line) for line in file]
+        return data
 
-
-def json_writer(data: object, params: JsonWriterParams) -> None:
-    with open(params.path, "w", encoding=params.encoding) as file:
-        json.dump(data, file, indent=params.indent)
-
-
-def multi_line_json_reader(params: JsonReaderParams) -> list[dict[str, str]]:
-    with open(params.path, "r", encoding=params.encoding) as file:
-        data = [json.loads(line) for line in file]
-    return data
+    parameters = JsonReaderParams
 
 
 _TModel = TypeVar("_TModel", bound=pydantic.BaseModel)
-
-
-@dataclasses.dataclass
-class MultiLinePydanticModelReaderParams(FilePathBaseParam, Generic[_TModel]):
-    model: Type[_TModel]
-    encoding: str = "UTF-8"
-
-
-def multi_line_pydantic_model_reader(params: MultiLinePydanticModelReaderParams[_TModel]) -> List[_TModel]:
-    with open(params.path, "r", encoding=params.encoding) as file:
-        model_ls = [params.model.model_validate_json(line) for line in file]
-    return model_ls
-
-
-@dataclasses.dataclass
-class MultiLinePydanticModelDfReaderParams(FilePathBaseParam, Generic[_TModel]):
-    model: Type[_TModel]
-    encoding: str = "UTF-8"
-    index: Optional[str] = None
-    rename_columns: Optional[dict[str, str]] = None
-
-
-def multi_line_pydantic_model_df_reader(params: MultiLinePydanticModelDfReaderParams[_TModel]) -> pd.DataFrame:
-    with open(params.path, "r", encoding=params.encoding) as file:
-        model_ls = pd.DataFrame([params.model.model_validate_json(line).model_dump() for line in file])
-    if params.rename_columns is not None:
-        model_ls.rename(columns=params.rename_columns, inplace=True)
-    if params.index is not None:
-        model_ls.set_index(params.index, inplace=True)
-    return model_ls
 
 
 @dataclasses.dataclass
@@ -78,6 +45,38 @@ class PydanticModelReaderParams(FilePathBaseParam, Generic[_TModel]):
     encoding: str = "UTF-8"
 
 
-def pydantic_model_reader(params: PydanticModelReaderParams[_TModel]) -> _TModel:
+class PydanticModel(DataStream[_TModel, PydanticModelReaderParams[_TModel]]):
+    @staticmethod
+    def _reader(params: PydanticModelReaderParams[_TModel]) -> _TModel:
+        with open(params.path, "r", encoding=params.encoding) as file:
+            return params.model.model_validate_json(file.read())
+
+    parameters = PydanticModelReaderParams
+
+
+@dataclasses.dataclass
+class MultiLinePydanticModelReaderParams(FilePathBaseParam, Generic[_TModel]):
+    model: Type[_TModel]
+    encoding: str = "UTF-8"
+    index: Optional[str] = None
+    column_names: Optional[dict[str, str]] = None
+
+
+class MultiLinePydanticModel(DataStream[pd.DataFrame, MultiLinePydanticModelReaderParams[_TModel]]):
+    @staticmethod
+    def _reader(params: MultiLinePydanticModelReaderParams[_TModel]) -> pd.DataFrame:
+        with open(params.path, "r", encoding=params.encoding) as file:
+            model_ls = pd.DataFrame([params.model.model_validate_json(line).model_dump() for line in file])
+        if params.column_names is not None:
+            model_ls.rename(columns=params.column_names, inplace=True)
+        if params.index is not None:
+            model_ls.set_index(params.index, inplace=True)
+        return model_ls
+
+    parameters = MultiLinePydanticModelReaderParams
+
+
+def multi_line_pydantic_model_reader(params: MultiLinePydanticModelReaderParams[_TModel]) -> List[_TModel]:
     with open(params.path, "r", encoding=params.encoding) as file:
-        return params.model.model_validate_json(file.read())
+        model_ls = [params.model.model_validate_json(line) for line in file]
+    return model_ls
