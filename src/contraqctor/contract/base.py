b78ff10e919c89eed10e486ea9eab697b1ebae77
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-05-26 11:39:25 -0700

    Iter now runs strictly on the level below.

--- src/contraqctor/contract/base.py
+++ src/contraqctor/contract/base.py
@@ -398,10 +398,14 @@ class DataStreamCollectionBase(
             DataStream: Child data streams.
         """
         for value in self._hashmap.values():
+            yield value
+
+    def iter_all(self) -> Generator[DataStream, None, None]:
+        for value in self:
             if isinstance(value, DataStream):
                 yield value
             if isinstance(value, DataStreamCollectionBase):
-                yield from value.__iter__()
+                yield from value.iter_all()
 
 
 class DataStreamCollection(DataStreamCollectionBase[DataStream, _typing.UnsetParamsType]):
